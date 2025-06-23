07e1ddbffc40ac0f3f6c28eafdd805f5705c6352
fmacedosantos <felipemacedo986@gmail.com>
2025-06-23 19:53:25 -0300

    Criando um filtro para o Token, validando e autenticando o usuário

--- src/main/java/br/com/forum_hub/infra/seguranca/ConfiguracoesSeguranca.java
+++ src/main/java/br/com/forum_hub/infra/seguranca/ConfiguracoesSeguranca.java
@@ -10,18 +10,32 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class ConfiguracoesSeguranca {
 
+    private final FiltroTokenAcesso filtroTokenAcesso;
+
+    public ConfiguracoesSeguranca(FiltroTokenAcesso filtroTokenAcesso) {
+        this.filtroTokenAcesso = filtroTokenAcesso;
+    }
+
     @Bean
     public SecurityFilterChain filtrosSeguranca(HttpSecurity http) throws Exception {
         return http
+                .authorizeHttpRequests(
+                        req -> {
+                            req.requestMatchers("/login").permitAll();
+                            req.anyRequest().authenticated();
+                        }
+                )
                 .sessionManagement(
                         sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS
                         ))
                 .csrf(csrf -> csrf.disable())
+                .addFilterBefore(filtroTokenAcesso, UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
 
